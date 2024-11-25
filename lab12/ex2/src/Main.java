27f99026eb3acf4c20db1e1c5592d47e639c952b
Ars5njo <a.latipov@innopolis.university>
2024-11-25 21:30:58 +0300

    update for lab12 ex2

--- lab12/ex2/src/Main.java
+++ lab12/ex2/src/Main.java
@@ -2,12 +2,15 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        try(FileInputStream in = new FileInputStream("/Users/arsen/InnopolisBS/ITP/labs/lab12/ex2/.idea/input.txt");
-            FileOutputStream out = new FileOutputStream("/Users/arsen/InnopolisBS/ITP/labs/lab12/ex2/.idea/output.txt"))
+        try(FileInputStream in = new FileInputStream("input.txt");
+            FileOutputStream out = new FileOutputStream("output.txt"))
         {
+            //The code read and write only one line from file
             byte[] buffer = new byte[in.available()];
             in.read(buffer, 0, buffer.length);
             out.write(buffer, 0, buffer.length);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
